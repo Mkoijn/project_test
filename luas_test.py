154ab865ef67019592cdc3ab0cd8b28b765c0835
Paul Durack <pdurack@gmail.com>
2018-02-25 10:05:37 +0000

    move coordinates

--- luas_test.py
+++ luas_test.py
@@ -1,34 +1,22 @@
 import luas.api
 from luas.api import LuasDirection
-from math import cos, asin, sqrt
 from geopy.geocoders import Nominatim
+import coordinates as co
 
 geolocator = Nominatim()
-location = geolocator.geocode("Kevin St Dublin 2")
+location = geolocator.geocode("19 Ebenezer Terrace")
 print(location.address)
 my_lat = location.latitude
 my_lon = location.longitude
 print(my_lat, my_lon)
 
-locs = [{'stop_name': 'FOU', 'lat': 53.346421, 'lon': -6.273455},
-        {'stop_name': 'FAT', 'lat': 53.338248, 'lon': -6.294419},
-        {'stop_name': 'RED', 'lat': 53.318460, 'lon': -6.373240}]
-
-
-def distance(lat1, lon1, lat2, lon2):
-    p = 0.017453292519943295
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) \
-        * (1-cos((lon2-lon1)*p)) / 2
-    return 12742 * asin(sqrt(a))
-
-
-def closest(data, v):
-    return min(data, key=lambda p: distance(v['lat'],
-                                            v['lon'], p['lat'], p['lon']))
+stops = [{'stop_name': 'FOU', 'lat': 53.346421, 'lon': -6.273455},
+         {'stop_name': 'FAT', 'lat': 53.338248, 'lon': -6.294419},
+         {'stop_name': 'RED', 'lat': 53.318460, 'lon': -6.373240}]
 
 
 v = {'lat': my_lat, 'lon': my_lon}
-closest_stop = closest(locs, v)
+closest_stop = co.closest(stops, v)
 print(closest_stop)
 
 
