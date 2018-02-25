56d4cf147dacc9d8cbb90edbf5464c2318a22236
Paul Durack <pdurack@gmail.com>
2018-02-25 10:20:31 +0000

    add gitignore

--- coordinates.py
+++ coordinates.py
@@ -2,6 +2,11 @@ from math import cos, asin, sqrt
 
 
 def distance(lat1, lon1, lat2, lon2):
+    '''
+    The haversine formula determines the great-circle
+    distance between two points on a sphere given
+    their longitudes and latitudes.
+    '''
     p = 0.017453292519943295
     a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) \
         * (1-cos((lon2-lon1)*p)) / 2
